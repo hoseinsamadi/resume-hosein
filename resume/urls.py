9b92e58716cb8d391b60e7efac4b2cf39ecd3c08
BiBak <Husein.samadi@gmail.com>
2024-10-01 15:22:06 +0330

    add urls and views website

--- resume/urls.py
+++ resume/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from resume.views import http_index
+from resume.views import *
 
 urlpatterns = [
     path('',http_index),
+    path('about',http_about),
+    path('/contact',http_contact),
+    path('/education',http_education),
+    path('/portfolio-details',http_portfolioDetails),
+    path('/portfolio',http_portfolio),
+    path('/pricing',http_pricing),
+    path('/service',http_service),
 ]
--- resume/views.py
+++ resume/views.py
@@ -4,3 +4,23 @@ from django.http import HttpRequest, HttpResponse, JsonResponse
 def http_index(request):
     return render(request,'website/index.html')
 
+def http_about(request):
+    return render(request,'website/about.html')
+
+def http_contact(request):
+    return render(request,'website/contact.html')
+
+def http_education(request):
+    return render(request,'website/education.html')
+
+def http_portfolioDetails(request):
+    return render(request,'website/portfolio-details.html')
+
+def http_portfolio(request):
+    return render(request,'website/portfolio.html')
+
+def http_pricing(request):
+    return render(request,'website/pricing.html')
+
+def http_service(request):
+    return render(request,'website/service.html')
