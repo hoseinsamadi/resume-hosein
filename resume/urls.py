dbea2f57c65c0c62381146087e3c4511a673f421
BiBak <Husein.samadi@gmail.com>
2024-10-01 15:25:12 +0330

    change urls

--- resume/urls.py
+++ resume/urls.py
@@ -5,10 +5,10 @@ from resume.views import *
 urlpatterns = [
     path('',http_index),
     path('about',http_about),
-    path('/contact',http_contact),
-    path('/education',http_education),
-    path('/portfolio-details',http_portfolioDetails),
-    path('/portfolio',http_portfolio),
-    path('/pricing',http_pricing),
-    path('/service',http_service),
+    path('contact',http_contact),
+    path('education',http_education),
+    path('portfolio-details',http_portfolioDetails),
+    path('portfolio',http_portfolio),
+    path('pricing',http_pricing),
+    path('service',http_service),
 ]
