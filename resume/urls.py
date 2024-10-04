280883df565470f8d4bb526707ba10efb2dca9d5
HoseinBiBak <husein.samadi@gmail.com>
2024-10-04 23:01:27 +0330

    edit and added dynamic urls

--- resume/urls.py
+++ resume/urls.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from django.urls import path
 from resume.views import *
 
+app_name = 'resume'
+
 urlpatterns = [
-    path('',http_index),
-    path('about',http_about),
-    path('contact',http_contact),
-    path('education',http_education),
-    path('portfolio-details',http_portfolioDetails),
-    path('portfolio',http_portfolio),
-    path('pricing',http_pricing),
-    path('service',http_service),
+    path('',http_index, name='index'),
+    path('about',http_about, name='about'),
+    path('contact',http_contact, name='contact'),
+    path('education',http_education, name='eduction'),
+    path('portfolio-details',http_portfolioDetails, name='portfolioDetails'),
+    path('portfolio',http_portfolio, name='portfolio'),
+    path('pricing',http_pricing, name='pricing'),
+    path('service',http_service, name='service'),
 ]
