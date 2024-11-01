46c39b1f66407f35d99dfa86a44bfc48679b23d4
HoseinBiBak <husein.samadi@gmail.com>
2024-11-01 22:18:02 +0330

    make migrations protfolio

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Portfolio(models.Model):
+    # author_portfolio
+    # image_portfolio
+    title_portfolio = models.CharField(max_length=255)
+    content_portfolio = models.TextField(null=True)
+    # category_id
+    # tage_id
+    content_portfolio = models.TextField(null=True)
+    status_portfolio = models.BooleanField(default=False)
+    published_date_portfolio = models.DateTimeField(null=True)
+    created_date_portfolio = models.DateTimeField(auto_now_add=True, null=True)
+    updates_date_portfolio = models.DateTimeField(auto_now_add=True, null=True)
