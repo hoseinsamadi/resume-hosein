40330c4c653ded2ee9c5fca9ba5ab9ca524e99a0
BiBak <Husein.samadi@gmail.com>
2024-11-03 15:55:47 +0330

    edit list_display

--- blog/admin.py
+++ blog/admin.py
@@ -5,6 +5,6 @@ class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     #fields = ('title','published_date',)
     empty_value_display = '-empty-'
-    list_display = ('title','counted_views','status','published_date','created_date')
+    list_display =["title","counted_view"]
 
 admin.site.register(Post)
