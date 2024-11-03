bef977c0425c742cc55b7e6f03129123ba219ab1
BiBak <Husein.samadi@gmail.com>
2024-11-03 16:01:25 +0330

    change PostAdmin

--- blog/admin.py
+++ blog/admin.py
@@ -5,6 +5,7 @@ class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     #fields = ('title','published_date',)
     empty_value_display = '-empty-'
-    list_display =["title","counted_view"]
+    #list_display =["title","counted_view"]
+    #list_filter = ('status')
 
-admin.site.register(Post)
+admin.site.register(Post,PostAdmin)
