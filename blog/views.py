854eb269082e5a702e6bac4e6b286475cf436e6a
BiBak <Husein.samadi@gmail.com>
2024-11-04 14:51:25 +0330

    added models to blog

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
+from blog.models import Post
 # Create your views here.
 def http_blog(request):
-    return render(request,'blog/blog.html')
+    posts = Post.objects.filter(status = 1)
+    context = {'posts': posts}
+    return render(request,'blog/blog.html', context)
 
 def http_blogDetails(request):
     return render(request,'blog/blog-details.html')
